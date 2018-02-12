ba521ee30413e2aaec0e3a7b6670e582863301e5
liuyi-victor <35279100+liuyi-victor@users.noreply.github.com>
2018-02-12 16:39:33 -0500

    commented out the extra fields in page struct

--- assignment1/library.h
+++ assignment1/library.h
@@ -10,8 +10,8 @@ typedef struct {
 	void *data;
 	int page_size;
 	int slot_size;
-	int bitmapLength;
-	int used_slots;
+	//int bitmapLength;
+	//int used_slots;
 } Page;
 // implement serialization of fixed length records.
 
